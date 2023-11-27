08dfd93c2b4670f2c51b6077a44e0af5024d09a7
JoseMariTan <132253808+JoseMariTan@users.noreply.github.com>
2023-11-27 09:32:23 +0800

    Update PopularB.dart

--- PopularB.dart
+++ PopularB.dart
@@ -1,4 +1,4 @@
-// Favorites.dart
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'FavoriteBarbers.dart';
 
@@ -10,143 +10,168 @@ class PopularB extends StatefulWidget {
 class _PopularBState extends State<PopularB> {
   final List<String> popularBarbersImages = [
     "https://www.nme.com/wp-content/uploads/2021/04/zild-benitez-credit-shaira-luna@2000x1270.jpg",
-    "https://i.scdn.co/image/ab6761610000e5ebc4902f080d3620b3e6da80c3",
+    "https://musicfeeds.com.au/wp-content/uploads/sites/7/2021/12/lordefallen.jpeg",
     "https://www.thepinknews.com/wp-content/uploads/2023/04/Frank-Ocean-pulls-out-of-Coachella-weekend-two.-Getty.jpg?w=1024",
     "https://nationaltoday.com/wp-content/uploads/2022/10/456840979-min-1200x834.jpg",
-    "https://highprofiles.info/wp-content/uploads/2016/04/Yorke-main-900x600.jpg",
+    "https://www.nme.com/wp-content/uploads/2022/10/shnti-twitter@2000x1270.jpg",
   ];
 
   List<bool> isFavoriteList = List.generate(5, (index) => false);
   List<bool> isInfoVisibleList = List.generate(5, (index) => false);
+  List<double> ratings = List.generate(5, (index) => 4 + Random().nextDouble());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Grabarber",
+          "Popular Barbers",
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Color(0xFF212121), // Match the color scheme
       ),
       body: Container(
         color: Color(0xFFE0E0E0),
-        child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  "Popular Barbers",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
               SizedBox(height: 10),
-              Container(
-                height: 500,
+              Expanded(
                 child: ListView.builder(
                   scrollDirection: Axis.vertical,
                   itemCount: popularBarbersImages.length,
                   itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        // Handle tap on the image
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Stack(
-                            alignment: Alignment.topRight,
-                            children: [
-                              Image.network(
-                                popularBarbersImages[index],
-                                width: double.infinity,
-                                height: 500.0,
-                                fit: BoxFit.cover,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    IconButton(
-                                      iconSize: 40,
-                                      icon: Icon(
-                                        Icons.star,
-                                        color: isFavoriteList[index]
-                                            ? Colors.yellow
-                                            : Colors.white,
-                                      ),
-                                      onPressed: () {
-                                        setState(() {
-                                          isFavoriteList[index] =
-                                              !isFavoriteList[index];
-                                          _handleFavorite(index);
-                                          _showMessage(
-                                              isFavoriteList[index]
-                                                  ? 'Added to Favorites'
-                                                  : 'Removed from Favorites',
-                                              context);
-                                        });
-                                      },
-                                    ),
-                                    IconButton(
-                                      iconSize: 40,
-                                      icon: Icon(
-                                        Icons.info,
-                                        color: Colors.white,
-                                      ),
-                                      onPressed: () {
-                                        setState(() {
-                                          isInfoVisibleList[index] =
-                                              !isInfoVisibleList[index];
-                                        });
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              if (isInfoVisibleList[index])
-                                Positioned(
-                                  bottom: 8.0,
-                                  left: 8.0,
-                                  child: Container(
-                                    padding: EdgeInsets.all(8.0),
-                                    decoration: BoxDecoration(
-                                      color: Colors.black.withOpacity(0.7),
-                                      borderRadius: BorderRadius.circular(5),
-                                    ),
-                                    child: Row(
-                                      children: [
-                                        Icon(
-                                          Icons.attach_money,
-                                          color: Colors.white,
-                                        ),
-                                        SizedBox(width: 5),
-                                        Text(
-                                          "Average booking price: PHP 12345",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
+                    return _buildBarberItem(index);
                   },
                 ),
               ),
               SizedBox(height: 25),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBarberItem(int index) {
+    return GestureDetector(
+      onTap: () {
+        // Handle tap on the image
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Stack(
+            alignment: Alignment.topRight,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 200.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: Color(0xFF212121),
+                    width: 4,
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(6),
+                  child: Image.network(
+                    popularBarbersImages[index],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: Container(
+                  padding: const EdgeInsets.all(6.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF212121),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(8),
+                      topRight: Radius.circular(8),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        iconSize: 20,
+                        icon: Icon(
+                          Icons.star,
+                          color: isFavoriteList[index]
+                              ? Colors.yellow
+                              : Colors.white,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            isFavoriteList[index] = !isFavoriteList[index];
+                            _handleFavorite(index);
+                            _showMessage(
+                              isFavoriteList[index]
+                                  ? 'Added to Favorites'
+                                  : 'Removed from Favorites',
+                              context,
+                            );
+                          });
+                        },
+                      ),
+                      IconButton(
+                        iconSize: 20,
+                        icon: Icon(
+                          Icons.info,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            isInfoVisibleList[index] =
+                                !isInfoVisibleList[index];
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              if (isInfoVisibleList[index])
+                Positioned(
+                  bottom: 8.0,
+                  left: 8.0,
+                  child: Container(
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.7),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 16,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              "Avg Rating: ${ratings[index].toStringAsFixed(1)}/5",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
             ],
           ),
         ),
