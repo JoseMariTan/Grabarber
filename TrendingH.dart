984fed374036e47ece7c9816b71d9b7321fb78af
JoseMariTan <132253808+JoseMariTan@users.noreply.github.com>
2023-11-27 09:32:00 +0800

    Update TrendingH.dart

--- TrendingH.dart
+++ TrendingH.dart
@@ -1,4 +1,4 @@
-// TrendingH.dart
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'FavoriteStyles.dart';
 
@@ -9,158 +9,202 @@ class TrendingH extends StatefulWidget {
 
 class _TrendingHState extends State<TrendingH> {
   final List<String> trendingHairstylesImages = [
-    "https://lovehairstyles.com/wp-content/uploads/2017/07/best-mens-hairstyles-ryan-reynolds-683x1024.jpg",
-    "https://menshairstyletips.com/wp-content/uploads/2020/11/Korean-Men-Haircut.jpg?resize=200%2C600",
-    "https://www.fashionbeans.com/wp-content/uploads/2021/11/Long-Hair-Undercut-and-Fade_longhairstylemen.jpg?resize=200%2C600",
-    "https://cdn.wisebarber.com/wisebarber/images/zw9hmzih431h1zr.jpg?width=432&height=576&aspect_ratio=432%3A576&quality=90",
+    "https://www.dmarge.com/wp-content/uploads/2021/05/mens-taper-fade-hairstyles-feature.jpg",
+    "https://mcdn.coolmate.me/image/August2022/toc-two-block-nam-3.jpg",
+    "https://www.chapsandco.com/ae/wp-content/uploads/sites/12/2023/02/tapered.jpg",
+    "https://images.tokopedia.net/img/KRMmCm/2022/9/22/4e4dd715-6b1c-4e69-a3c6-c27dd44f27fa.jpg",
   ];
 
   List<bool> isFavoriteList = List.generate(4, (index) => false);
-
   List<bool> isInfoClickedList = List.generate(4, (index) => false);
-
   List<bool> isAddedToFavoritesList = List.generate(4, (index) => false);
+  List<double> ratings = List.generate(4, (index) => 4 + Random().nextDouble());
+  List<String> hairstyleNames = [
+    "Taper",
+    "Two Block",
+    "Buzzcut",
+    "French Crop"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Grabarber",
+          "Trending Hairstyles",
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Color(0xFF212121), // Match the color scheme
       ),
       body: Container(
         color: Color(0xFFE0E0E0),
-        child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  "Trending Hairstyles",
-                  style: TextStyle(
-                    color: Colors.black, // Updated text color
-                    fontSize: 24,
-                  ),
-                ),
-              ),
               SizedBox(height: 10),
-              Container(
-                height: 600,
+              Expanded(
                 child: ListView.builder(
                   scrollDirection: Axis.vertical,
                   itemCount: trendingHairstylesImages.length,
                   itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        // Handle tap on the image
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Stack(
-                            alignment: Alignment.topRight,
-                            children: [
-                              Image.network(
-                                trendingHairstylesImages[index],
-                                width: 1000.0,
-                                height: 600.0,
-                                fit: BoxFit.cover,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    IconButton(
-                                      iconSize: 40,
-                                      icon: Icon(
-                                        Icons.star,
-                                        color: isFavoriteList[index]
-                                            ? Colors.yellow
-                                            : Colors.white,
-                                      ),
-                                      onPressed: () {
-                                        setState(() {
-                                          isFavoriteList[index] =
-                                              !isFavoriteList[index];
-                                          _handleFavorite(index);
-                                          _showMessage(
-                                            isFavoriteList[index]
-                                                ? 'Added to Favorites'
-                                                : 'Removed from Favorites',
-                                            context,
-                                          );
-                                        });
-                                      },
-                                    ),
-                                    IconButton(
-                                      iconSize: 40,
-                                      icon: Icon(
-                                        Icons.info,
-                                        color: Colors.white,
-                                      ),
-                                      onPressed: () {
-                                        setState(() {
-                                          isInfoClickedList[index] =
-                                              !isInfoClickedList[index];
-                                        });
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              if (isInfoClickedList[index])
-                                Positioned(
-                                  top: 50,
-                                  right: 50,
-                                  child: Container(
-                                    width: 150,
-                                    height: 80,
-                                    color: Colors.black.withOpacity(0.7),
-                                    child: Center(
-                                      child: Text(
-                                        "Info",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              if (isAddedToFavoritesList[index])
-                                Positioned(
-                                  top: 50,
-                                  left: 50,
-                                  child: Container(
-                                    width: 200,
-                                    height: 50,
-                                    color: Colors.black.withOpacity(0.7),
-                                    child: Center(
-                                      child: Text(
-                                        "Added to Favorites",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
+                    return _buildHairstyleItem(index);
                   },
                 ),
               ),
               SizedBox(height: 25),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHairstyleItem(int index) {
+    return GestureDetector(
+      onTap: () {
+        // Handle tap on the image
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Stack(
+            alignment: Alignment.topRight,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 200.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: Color(0xFF212121),
+                    width: 4,
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(6),
+                  child: Image.network(
+                    trendingHairstylesImages[index],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: Container(
+                  padding: const EdgeInsets.all(6.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF212121),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(8),
+                      topRight: Radius.circular(8),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        iconSize: 20,
+                        icon: Icon(
+                          Icons.star,
+                          color: isFavoriteList[index]
+                              ? Colors.yellow
+                              : Colors.white,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            isFavoriteList[index] = !isFavoriteList[index];
+                            _handleFavorite(index);
+                            _showMessage(
+                              isFavoriteList[index]
+                                  ? 'Added to Favorites'
+                                  : 'Removed from Favorites',
+                              context,
+                            );
+                          });
+                        },
+                      ),
+                      IconButton(
+                        iconSize: 20,
+                        icon: Icon(
+                          Icons.info,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            isInfoClickedList[index] =
+                                !isInfoClickedList[index];
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              if (isInfoClickedList[index])
+                Positioned(
+                  bottom: 8.0,
+                  left: 8.0,
+                  child: Container(
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.7),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 16,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              "${ratings[index].toStringAsFixed(1)}/5",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          "Hairstyle: ${hairstyleNames[index]}",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              if (isAddedToFavoritesList[index])
+                Positioned(
+                  top: 50,
+                  left: 50,
+                  child: Container(
+                    width: 200,
+                    height: 50,
+                    color: Colors.black.withOpacity(0.7),
+                    child: Center(
+                      child: Text(
+                        "Added to Favorites",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
             ],
           ),
         ),
